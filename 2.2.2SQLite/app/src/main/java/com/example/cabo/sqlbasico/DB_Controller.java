6bcf6f0e5d698a4df23584e17ef9cce302c6b989
Dylan Salvador Parra Dominguez <dysaparrado@ittepic.edu.mx>
2018-02-27 23:36:09 -0700

    2.2.2SQLite

---
+++ 2.2.2SQLite/app/src/main/java/com/example/cabo/sqlbasico/DB_Controller.java
@@ -0,0 +1,53 @@
+package com.example.cabo.sqlbasico;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.TextView;
+
+/**
+ * Created by Cabo on 26/02/2018.
+ */
+
+public class DB_Controller extends SQLiteOpenHelper {
+    public DB_Controller(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, "PruebaBase.db", factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("CREATE TABLE Users(ID INTEGER PRIMARY KEY AUTOINCREMENT, USUARIO TEXT UNIQUE, PASSWORD TEXT); ");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS Users;");
+        onCreate(sqLiteDatabase);
+    }
+
+    public void insertar_user(String USUARIO, String PASSWORD){
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("USUARIO",USUARIO);
+        contentValues.put("PASSWORD",PASSWORD);
+        this.getWritableDatabase().insertOrThrow("Users","",contentValues);
+    }
+
+    public void borrar_user(String USUARIO){
+        this.getWritableDatabase().delete("Users","USUARIO='"+USUARIO+"'",null);
+    }
+
+    public  void actualizar(String viejo_USUARIO, String nuevo_USUARIO){
+        this.getWritableDatabase().execSQL("UPDATE Users SET USUARIO='"+nuevo_USUARIO+"' WHERE USUARIO='"+viejo_USUARIO+"'");
+    }
+
+    public  void show_users(TextView textView){
+        Cursor cursor = this.getReadableDatabase().rawQuery("SELECT * FROM Users",null);
+        textView.setText("");
+        while(cursor.moveToNext()){
+            textView.append(cursor.getString(1)+" "+cursor.getString(2)+"\n");
+        }
+    }
+
+}
---
+++ 2.2.2SQLite/app/src/main/java/com/example/cabo/sqlbasico/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.cabo.sqlbasico;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.sql.SQLException;
+
+public class MainActivity extends AppCompatActivity {
+    EditText usuario,contra;
+    TextView vista;
+    Button añadir,borrar,actualizar,mostrar;
+    DB_Controller controller;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        usuario = findViewById(R.id.Usuario);
+        contra = findViewById(R.id.Password);
+        vista = findViewById(R.id.show);
+        añadir = findViewById(R.id.añadir);
+        borrar = findViewById(R.id.borrar);
+        //actualizar = findViewById(R.id.actualizar);
+        mostrar = findViewById(R.id.mostrar);
+
+        controller = new DB_Controller(this,"",null,1);
+
+
+        añadir.setOnClickListener(new View.OnClickListener() {
+
+
+
+                public void onClick (View v){
+
+                controller.insertar_user(usuario.getText().toString(), contra.getText().toString());
+        usuario.setText("");
+        contra.setText("");
+            }
+
+
+        });
+
+        borrar.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                controller.borrar_user(usuario.getText().toString());
+                usuario.setText("");
+                contra.setText("");
+            }
+        });
+
+        /*actualizar.setOnClickListener(new View.OnClickListener() {
+            String txttemp;
+            public void onClick(View v) {
+                txttemp = contra.getText().toString();
+                controller.actualizar(contra.getText().toString(),usuario.getText().toString());
+                usuario.setText("");
+                contra.setText("");
+            }
+        });
+*/
+        mostrar.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                controller.show_users(vista);
+                usuario.setText("");
+                contra.setText("");
+            }
+        });
+    }
+}
+
+
